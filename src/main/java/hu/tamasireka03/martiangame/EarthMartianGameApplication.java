7ba1509dbe6cc7fbe3efe1cdfdbe7f9bf8731b72
reka03 <tamasireka03@gmail.com>
2025-08-06 21:29:49 +0200

    Initial commit: Spring Boot project setup with martiangame database

---
+++ src/main/java/hu/tamasireka03/martiangame/EarthMartianGameApplication.java
@@ -0,0 +1,13 @@
+package hu.tamasireka03.martiangame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthMartianGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthMartianGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/tamasireka03/martiangame/EarthMartianGameApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.tamasireka03.martiangame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthMartianGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
